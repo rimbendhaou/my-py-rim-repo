a7dea164eb95ff846aedeaad71996ff4c62ec839
rim <bendhaou.rim@gmail.com>
2020-04-13 15:25:39 -0400

    my circle ci integration

---
+++ setup.py
@@ -0,0 +1,22 @@
+# setup.py
+from setuptools import find_packages, setup
+
+
+setup(
+    name='my-py-rim-repo',
+    version='0.0.1',
+    python_requires='>=3.6,<=3.8',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 1 - Planning',
+
+        'Intended Audience :: Developers',
+
+        'Programming Language :: Python :: 3.6',
+    ],
+    author='Rim BEN DHAOU',
+    author_email='bendhaou.rim@gmail.com',
+    license='MIT',
+    url='https://github.com/rimbendhaou/my-py-rim-repo.git',
+)
+
